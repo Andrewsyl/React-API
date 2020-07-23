8272c685f57c8cd6e326dbd5e3bc1c47a3b1b43f
unknown <andrewsylcostello@gmail.com>
2020-07-23 11:51:08 +0100

    update

--- src/components/city_form.js
+++ src/components/city_form.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const Form = (props) => {
   return (
-    <form onSubmit={props.loadweather}>
-      <input type="text" className="form" name="city" placeholder="City" />
-      <input
-        type="text"
-        className="form"
-        name="country"
-        placeholder="Country"
-      />
-      <button>Submit</button>
-    </form>
+    <div className="py-5">
+      <form onSubmit={props.loadweather}>
+        <input type="text" className="form" name="city" placeholder="City" />
+        <input
+          type="text"
+          className="form"
+          name="country"
+          placeholder="Country"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
